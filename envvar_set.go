010a4f98d88d812ed32772e8f8c9ce6661be4608
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2018-06-04 15:26:46 +0200

    Add environment variable set structure

---
+++ envvar_set.go
@@ -0,0 +1,10 @@
+package env
+
+// EnvVarSet is a set of defined environment variables.
+type EnvVarSet struct {
+}
+
+// NewEnvVarSet returns a new, empty environment variable set.
+func NewEnvVarSet() *EnvVarSet {
+	return &EnvVarSet{}
+}
---
+++ envvar_set_test.go
@@ -0,0 +1,11 @@
+package env_test
+
+import (
+	"testing"
+
+	"github.com/goph/env"
+)
+
+func TestEnvVarSet(t *testing.T) {
+	_ = env.NewEnvVarSet()
+}
