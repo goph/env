ca187f867faf2d77a6af4c3116b248de77177944
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2018-06-05 17:40:14 +0200

    Add int32 type value

---
+++ value_int32.go
@@ -0,0 +1,39 @@
+package env
+
+import "strconv"
+
+type int32Value int32
+
+func newInt32Value(val int32, p *int32) *int32Value {
+	*p = val
+
+	return (*int32Value)(p)
+}
+
+func (i *int32Value) Set(val string) error {
+	v, err := strconv.ParseInt(val, 0, 32)
+
+	*i = int32Value(v)
+
+	return err
+}
+
+func (*int32Value) Type() string { return "int32" }
+
+func (i *int32Value) String() string { return strconv.FormatInt(int64(*i), 10) }
+
+// Int32Var defines an int32 environment variable with specified name, default value, and usage string.
+// The argument p points to an int32 variable in which to store the value of the environment variable.
+func (s *EnvVarSet) Int32Var(p *int32, name string, value int32, usage string) {
+	s.Var(newInt32Value(value, p), name, usage)
+}
+
+// Int32 defines an int32 environment variable with specified name, default value, and usage string.
+// The return value is the address of an int32 variable that stores the value of the environment variable.
+func (s *EnvVarSet) Int32(name string, value int32, usage string) *int32 {
+	p := new(int32)
+
+	s.Int32Var(p, name, value, usage)
+
+	return p
+}
--- value_test.go
+++ value_test.go
@@ -8,8 +8,9 @@ import (
 
 type valueVars struct {
 	intVar    *int
-	int16Var    *int16
-	int8Var    *int8
+	int16Var  *int16
+	int32Var  *int32
+	int8Var   *int8
 	stringVar *string
 }
 
@@ -19,6 +20,7 @@ func TestValue(t *testing.T) {
 
 	vars.intVar = envvarset.Int("int", 0, "int value")
 	vars.int16Var = envvarset.Int16("int16", 0, "int16 value")
+	vars.int32Var = envvarset.Int32("int32", 0, "int32 value")
 	vars.int8Var = envvarset.Int8("int8", 0, "int8 value")
 	vars.stringVar = envvarset.String("string", "", "string value")
 
@@ -29,13 +31,15 @@ func TestValueVar(t *testing.T) {
 	envvarset := env.NewEnvVarSet(env.ContinueOnError)
 	vars := &valueVars{
 		intVar:    new(int),
-		int16Var:    new(int16),
-		int8Var:    new(int8),
+		int16Var:  new(int16),
+		int32Var:  new(int32),
+		int8Var:   new(int8),
 		stringVar: new(string),
 	}
 
 	envvarset.IntVar(vars.intVar, "int", 0, "int value")
 	envvarset.Int16Var(vars.int16Var, "int16", 0, "int16 value")
+	envvarset.Int32Var(vars.int32Var, "int32", 0, "int32 value")
 	envvarset.Int8Var(vars.int8Var, "int8", 0, "int8 value")
 	envvarset.StringVar(vars.stringVar, "string", "", "string value")
 
@@ -45,8 +49,9 @@ func TestValueVar(t *testing.T) {
 func testValue(t *testing.T, envvarset *env.EnvVarSet, vars *valueVars) {
 	environment := map[string]string{
 		"int":    "22",
-		"int16":    "16",
-		"int8":    "8",
+		"int16":  "16",
+		"int32":  "32",
+		"int8":   "8",
 		"string": "string",
 	}
 
@@ -62,6 +67,10 @@ func testValue(t *testing.T, envvarset *env.EnvVarSet, vars *valueVars) {
 
 	if *vars.int16Var != 16 {
 		t.Error("int16 var should be `16`, got: ", *vars.int16Var)
+	}
+
+	if *vars.int32Var != 32 {
+		t.Error("int32 var should be `32`, got: ", *vars.int32Var)
 	}
 
 	if *vars.int8Var != 8 {
