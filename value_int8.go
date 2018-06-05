e49b59519e276622a44a9453e349b97994838442
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2018-06-05 17:33:49 +0200

    Add int8 type value

---
+++ value_int8.go
@@ -0,0 +1,39 @@
+package env
+
+import "strconv"
+
+type int8Value int8
+
+func newInt8Value(val int8, p *int8) *int8Value {
+	*p = val
+
+	return (*int8Value)(p)
+}
+
+func (i *int8Value) Set(val string) error {
+	v, err := strconv.ParseInt(val, 0, 8)
+
+	*i = int8Value(v)
+
+	return err
+}
+
+func (*int8Value) Type() string { return "int8" }
+
+func (i *int8Value) String() string { return strconv.FormatInt(int64(*i), 10) }
+
+// Int8Var defines a int8 environment variable with specified name, default value, and usage string.
+// The argument p points to a int8 variable in which to store the value of the environment variable.
+func (s *EnvVarSet) Int8Var(p *int8, name string, value int8, usage string) {
+	s.Var(newInt8Value(value, p), name, usage)
+}
+
+// Int8 defines a int8 environment variable with specified name, default value, and usage string.
+// The return value is the address of a int8 variable that stores the value of the environment variable.
+func (s *EnvVarSet) Int8(name string, value int8, usage string) *int8 {
+	p := new(int8)
+
+	s.Int8Var(p, name, value, usage)
+
+	return p
+}
--- value_test.go
+++ value_test.go
@@ -8,6 +8,7 @@ import (
 
 type valueVars struct {
 	intVar    *int
+	int8Var    *int8
 	stringVar *string
 }
 
@@ -16,6 +17,7 @@ func TestValue(t *testing.T) {
 	vars := new(valueVars)
 
 	vars.intVar = envvarset.Int("int", 0, "int value")
+	vars.int8Var = envvarset.Int8("int8", 0, "int8 value")
 	vars.stringVar = envvarset.String("string", "", "string value")
 
 	testValue(t, envvarset, vars)
@@ -25,10 +27,12 @@ func TestValueVar(t *testing.T) {
 	envvarset := env.NewEnvVarSet(env.ContinueOnError)
 	vars := &valueVars{
 		intVar:    new(int),
+		int8Var:    new(int8),
 		stringVar: new(string),
 	}
 
 	envvarset.IntVar(vars.intVar, "int", 0, "int value")
+	envvarset.Int8Var(vars.int8Var, "int8", 0, "int8 value")
 	envvarset.StringVar(vars.stringVar, "string", "", "string value")
 
 	testValue(t, envvarset, vars)
@@ -37,6 +41,7 @@ func TestValueVar(t *testing.T) {
 func testValue(t *testing.T, envvarset *env.EnvVarSet, vars *valueVars) {
 	environment := map[string]string{
 		"int":    "22",
+		"int8":    "8",
 		"string": "string",
 	}
 
@@ -48,6 +53,10 @@ func testValue(t *testing.T, envvarset *env.EnvVarSet, vars *valueVars) {
 
 	if *vars.intVar != 22 {
 		t.Error("int var should be `22`, got: ", *vars.intVar)
+	}
+
+	if *vars.int8Var != 8 {
+		t.Error("int8 var should be `8`, got: ", *vars.int8Var)
 	}
 
 	if *vars.stringVar != "string" {
